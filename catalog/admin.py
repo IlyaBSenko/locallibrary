3bda32a484e6d7be921b5fbd56d564a28aa50254
Ilya Balatsenko <balatsir@dukes.jmu.edu>
2026-09-09 19:17:09 -0400

    Fixed typo

--- catalog/admin.py
+++ catalog/admin.py
@@ -5,12 +5,17 @@ from .models import Author, Genre, Book, BookInstance, Language
 class BooksInstanceInline(admin.TabularInline):
     model = BookInstance
     extra = 0
+    
+class BooksInline(admin.TabularInline):
+    model = Book
+    extra = 0
 
 
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BooksInline]
 
 
 @admin.register(Book)
