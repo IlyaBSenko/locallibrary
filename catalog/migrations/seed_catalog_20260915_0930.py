4f99028d615a38a012e4e7a37331c1d5581652f8
Ilya Balatsenko <balatsir@dukes.jmu.edu>
2026-09-15 12:26:47 -0400

    minor changes

--- catalog/migrations/seed_catalog_20260915_0930.py
+++ catalog/migrations/seed_catalog_20260915_0930.py
@@ -170,7 +170,6 @@ def seed_books(apps, schema_editor):
 
 class Migration(migrations.Migration):
 
-    # FIXME: remove assert False and replace "000X_YOUR_MOST_RECENT_MIGRATION_NAME" with the name of your most recent migration file in the dependencies list below before running this migration
     dependencies = [
         ("catalog", "0001_initial"),
     ]
